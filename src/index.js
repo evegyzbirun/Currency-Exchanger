47c361b71d996567ada4e255ba3ffce665500de8
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-22 16:35:32 -0700

    fixing html and js files.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { getCurrency } from './service';
 
+//we need to take currency and its rate from API
+//input amount of money we want to convert 
+//pick the currency we need to concert in 
+//find based of selected valua
+//it will divide input on the current rate of the second currency
+//get output in #output
 
 function inputAndConvert() {
   $('#inputYourCurrency').number();
@@ -15,5 +21,12 @@ function displayAfterCovert() {
   $('#outputYourCurrency').val();
 };
 
-function displayData(data)
+function getRates(response) {
+  if (response) {
+    const pickedCurrency = (response)
+  }
+}
+
+
+function displayData()
 
