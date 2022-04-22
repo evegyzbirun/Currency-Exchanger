88bd378064c20d1474dafdbc9bb1d8dad857163e
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-04-22 10:26:26 -0700

    connecting API.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import $ from 'jquery';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+
+//select country currency
+const select = document.querySelectorAll("select");
+//input amount 
+const input = document.querySelectorAll("input");
+const api_utl = "https://v6.exchangerate-api.com/v6/5beb8f7b76cb87ab64732939/latest/USD";
+
+async function currency() {
+  let getAPI = await fetch(api_utl);
+  console.log(getAPI);
+
+}
+currency();
--- src/service.js
+++ src/service.js
@@ -1,4 +1,2 @@
-export default class Currency {
-  static getExchange()
-  let utl = `https://v6.exchangerate-api.com/v6/5beb8f7b76cb87ab64732939/latest/USD`
-}
+
+
